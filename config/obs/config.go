869784b707acf54a3261f5eff6b5a22f68eeb459
chengxiangdong <chengxiangdong@outlook.com>
2024-10-10 13:03:54 +0800

    feat: add import support

--- config/obs/config.go
+++ config/obs/config.go
@@ -1,22 +1,41 @@
 package obs
 
-import "github.com/crossplane/upjet/pkg/config"
+import (
+	"context"
+	"fmt"
+
+	"github.com/crossplane/upjet/pkg/config"
+)
 
 const shortGroupObs = "obs"
 
 // Configure configures individual resources by adding custom ResourceConfigurators.
 func Configure(p *config.Provider) {
 	p.AddResourceConfigurator("huaweicloud_obs_bucket", func(r *config.Resource) {
-		// We need to override the default group that upjet generated for
-		// this resource, which would be "obs"
 		r.ShortGroup = shortGroupObs
+		r.ExternalName = config.IdentifierFromProvider
 	})
 
 	p.AddResourceConfigurator("huaweicloud_obs_bucket_object", func(r *config.Resource) {
 		r.ShortGroup = shortGroupObs
+		r.ExternalName = config.IdentifierFromProvider
+		r.ExternalName.GetIDFn = getFullyQualifiedIDfunc
+
 		r.References["bucket"] = config.Reference{
 			TerraformName: "huaweicloud_obs_bucket",
 			Extractor:     `github.com/crossplane/upjet/pkg/resource.ExtractParamPath("bucket",true)`,
 		}
 	})
+}
+
+func getFullyQualifiedIDfunc(_ context.Context, _ string, _ map[string]any, providerConfig map[string]any) (string, error) {
+	bucket, ok := providerConfig["bucket"]
+	if !ok {
+		return "", fmt.Errorf("not found attribute bucket")
+	}
+	key, ok := providerConfig["key"]
+	if !ok {
+		return "", fmt.Errorf("not found attribute key")
+	}
+	return fmt.Sprintf("%s/%s", bucket, key), nil
 }
--- config/vpc/config.go
+++ config/vpc/config.go
@@ -1,6 +1,8 @@
 package vpc
 
-import "github.com/crossplane/upjet/pkg/config"
+import (
+	"github.com/crossplane/upjet/pkg/config"
+)
 
 const shortGroupVpc = "vpc"
 
@@ -10,11 +12,14 @@ func Configure(p *config.Provider) {
 		// We need to override the default group that upjet generated for
 		// this resource, which would be "vpc"
 		r.ShortGroup = shortGroupVpc
+		r.ExternalName = config.IdentifierFromProvider
 	})
 
 	p.AddResourceConfigurator("huaweicloud_vpc_subnet", func(r *config.Resource) {
 		// We need to override the default group that upjet generated for
 		r.ShortGroup = shortGroupVpc
+		r.ExternalName = config.IdentifierFromProvider
+
 		r.References["vpc_id"] = config.Reference{
 			TerraformName: "huaweicloud_vpc",
 			Extractor:     `github.com/crossplane/upjet/pkg/resource.ExtractParamPath("id",true)`,
